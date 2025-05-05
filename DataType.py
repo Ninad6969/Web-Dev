c08a3e2c4b170c98cba8201217d466c0927ce73a
Ninad <Ninad70707@gmail.com>
2025-05-05 21:53:51 +0600

    Add files via upload

---
+++ DataType.py
@@ -0,0 +1,46 @@
+#Data Type
+
+Name = "Name" #String
+Grade = 5 #Integer
+Marks = 98.3 # Float
+Pass = True #Boolean
+
+print(type(Name))
+print(type(Grade))
+print(type(Marks))
+print(type(Pass))
+
+a = 5
+b = 3
+#Arithmatic
+print(a+b)
+print(a*b)
+print(a-b)
+print(a/b)
+print(a**b)
+
+#Logical Ops
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+print(a==b)
+print(a!=b)
+
+text = "Codingal" #String
+
+#index
+print(text[0])
+print(text[1])
+print(text[2])
+print(text[3])
+print(text[4])
+print(text[5])
+print(text[6])
+print(text[7])
+print(text[8])
+
+#Basic Functions
+print(text.upper())
+print(text.lower())
+print(text.swapcase())
